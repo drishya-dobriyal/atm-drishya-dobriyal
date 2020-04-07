a3175c95390d33ccd7c38a89c50e90138d0b0f2f
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-04-07 15:52:30 +0530

    fixed a bug in get_money function

--- atm.h
+++ atm.h
@@ -3,14 +3,13 @@ unsigned int get_money(unsigned short int);
 unsigned int get_money(unsigned short int amount ) {
   unsigned  int curr_notes = 0;
   int notes[] = { 2000, 500, 100, 50, 20, 10, 5, 1};
-  int *note_ptr = notes;
-  int num = 8;
-  while( num != 0 ){
+  if( amount > 31999 ){
+    return curr_notes;
+  }
+  for( int i = 0; i < 8; i++) {
     curr_notes = curr_notes << 4;
-    curr_notes = curr_notes | amount / *note_ptr;
-    amount = amount % *note_ptr;
-    note_ptr++;
-    num--;
+    curr_notes = curr_notes | amount / notes[i];
+    amount = amount % notes[i];
   }
   return curr_notes;
 }
